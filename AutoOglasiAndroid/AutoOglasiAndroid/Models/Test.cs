d3599e96a117089280b1e02c8965a98eee33b604
nenad <nenad@DESKTOP-HR3TK8P>
2019-05-14 19:55:57 +0200

    Add project files.

---
+++ AutoOglasiAndroid/AutoOglasiAndroid/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoOglasiAndroid.Models
+{
+    class Test
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public Test()
+        {
+            Name = "Nenad Aleksic";
+            Age = "15";
+        }
+    }
+}
